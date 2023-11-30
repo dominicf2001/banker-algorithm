9edf72bb66e1f5967de15b04a208e3812aaef954
dominic <dominicf2001@gmail.com>
2023-11-30 16:46:05 -0500

    Incorrect logic fixes

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int** allocation;
 int** available;
-int** max;
+int* max;
 
 void initialize() {
     FILE *fp = fopen("input.txt", "r");
@@ -14,44 +14,50 @@ void initialize() {
     // determine num of resources and processes
     int num_of_resources = 0;
     int num_of_processes = 0;
-    int done_counting = 0;
+    int done_counting_resources = 0;
     while (c != '-'){
-        if (!done_counting && isdigit(c)) {
+        if (!done_counting_resources && isdigit(c))
             ++num_of_resources;
-        }
-        if (c == '|') {
-            done_counting = 1;
-        }
-        if (c == '\n') {
+        
+        if (c == '|')
+            done_counting_resources = 1;
+        
+        if (c == '\n')
             ++num_of_processes;
-        }
+        
         c = fgetc(fp);
     }
     fseek(fp, 0, SEEK_SET);
 
     // allocate memory for the matrices
-    allocation = malloc(sizeof(int) * (num_of_processes * num_of_resources));
-    available = malloc(sizeof(int) * (num_of_processes * num_of_resources));
+    allocation = malloc(sizeof(int*) * num_of_processes);
+    for (int i = 0; i < num_of_processes; ++i) {
+        allocation[i] = malloc(sizeof(int) * num_of_resources);
+    }
+    available = malloc(sizeof(int*) * num_of_processes);
+    for (int i = 0; i < num_of_processes; ++i) {
+        available[i] = malloc(sizeof(int) * num_of_resources);
+    }
     max = malloc(sizeof(int) * num_of_resources);
     
     // initialize available and allocation matrices
     int is_allocation = 0;
     int p_num = 0;
-    int i = 0;
-    while (c != '-' || !feof(fp)) {
+    int r_num = 0;
+    while (c != '-' && !feof(fp)) {
         if (is_allocation) {
-            allocation[p_num][i] = (c - '0');
+            allocation[p_num][r_num] = (c - '0');
         }
         else {
-            available[p_num][i] = (c - '0');
+            available[p_num][r_num] = (c - '0');
         }
         
         if (c == '|') {
             is_allocation = !is_allocation;
-            i = 0;
+            r_num = 0;
         }
         else {
-            ++i;
+            ++r_num;
         }
         
         if (c == '\n')
